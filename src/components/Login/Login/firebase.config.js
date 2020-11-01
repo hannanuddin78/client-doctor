953ddd5207b14f2a2febae543a515aab4888f2d3
hannanuddin78 <hannanuddin78@gmail.com>
2020-11-01 11:30:13 +0600

    finish

---
+++ src/components/Login/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDN1eML_FzIDVwMa9Ca-QhzeX6lzLMo_qw",
+  authDomain: "own-doctor-portal.firebaseapp.com",
+  databaseURL: "https://own-doctor-portal.firebaseio.com",
+  projectId: "own-doctor-portal",
+  storageBucket: "own-doctor-portal.appspot.com",
+  messagingSenderId: "461346352338",
+  appId: "1:461346352338:web:19067851105f1abe40910b",
+};
+export default firebaseConfig;
